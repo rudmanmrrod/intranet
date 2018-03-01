f8d210c69b958c5d9cbf4fd0d069e70662386986
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-03-01 13:40:41 -0400

    Añadido menú lateral que cambia de acuerdo al usuario

---
+++ base/templatetags/auth_extra.py
@@ -0,0 +1,17 @@
+"""
+Sistema de Intranet
+"""
+## @package base.templatetags.auth_extra
+#
+# Filtro extra para comprobar si un usuario esta en un grupo
+# @version 1.0
+
+from django import template
+from django.contrib.auth.models import Group 
+
+register = template.Library()
+
+@register.filter(name='has_group')
+def has_group(user, group_name): 
+    group = Group.objects.get(name=group_name) 
+    return True if group in user.groups.all() else False
